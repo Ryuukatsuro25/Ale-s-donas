4f19daaeb498ad269c150c4a3e253a6330d4e4f3
Ryuukatsuro25 <123609042+Ryuukatsuro25@users.noreply.github.com>
2025-05-10 12:07:36 -0500

    pruebas

--- js/table.js
+++ js/table.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
   const donuts = [
-    { name: 'Sencillas', description: '4 unidades',  price: '$5.500' },
-    { name: 'Sencillas', description: '8 unidades',  price: '$10.000' },
+    { name: 'Sencillas', description: '4 unidades', price: '$5.500' },
+    { name: 'Sencillas', description: '8 unidades', price: '$10.000' },
     { name: 'Sencillas', description: '12 unidades', price: '$15.000' },
     { name: 'Sencillas', description: '24 unidades', price: '$28.000' },
-    { name: 'Especiales', description: '4 unidades',  price: '$6.500' },
-    { name: 'Especiales', description: '8 unidades',  price: '$10.500' },
+    { name: 'Especiales', description: '4 unidades', price: '$6.500' },
+    { name: 'Especiales', description: '8 unidades', price: '$10.500' },
     { name: 'Especiales', description: '12 unidades', price: '$16.500' },
     { name: 'Especiales', description: '24 unidades', price: '$30.500' },
   ];
